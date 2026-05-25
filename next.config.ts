f3ef8c0623dabf0ba4681507327cfbdf8974b141
wynzyl <wynzyltayag@gmail.com>
2026-05-25 10:04:24 +0800

    add-cache-components-in-nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: 'standalone',
+  cacheComponents: true,
   allowedDevOrigins: [
     'http://localhost:3000',
     ...(process.env.APP_BASE_URL
